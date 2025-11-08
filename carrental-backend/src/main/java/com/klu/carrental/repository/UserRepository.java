f2156449e898eeabc979be015406c57122ce038a
balaji-0505 <pabbathibalaji12@gmail.com>
2025-11-08 15:28:38 +0530

    Update UserRepository.java

--- carrental-backend/src/main/java/com/klu/carrental/repository/UserRepository.java
+++ carrental-backend/src/main/java/com/klu/carrental/repository/UserRepository.java
@@ -1,13 +1,9 @@
 package com.klu.carrental.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.klu.carrental.entity.User;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
